95e157628172c5101a4ba2f68cb9a04ddcb5ea93
Kimberley Mackenzie <kimberley@pulumi.com>
2023-01-26 15:53:48 -0800

    First pass at updating API nav and removing facet.

--- themes/default/theme/src/ts/main.ts
+++ themes/default/theme/src/ts/main.ts
@@ -1,4 +1,3 @@
-import { initDesignSystem, button, accordion, accordionItem, disclosure, treeItem, treeView } from "@pulumi/facet";
 import { defineCustomElements } from "../../stencil/dist";
 
 import "../scss/main.scss";
@@ -20,12 +19,6 @@ import "./packages";
 import "./pricing-trial";
 import "./developer-advocates";
 import "./toc";
-
-// Initialize the Facet design system and components.
-initDesignSystem({
-    prefix: "pulumi",
-    components: [accordion(), accordionItem(), button(), disclosure(), treeItem(), treeView()],
-});
 
 // Register all Stencil components.
 defineCustomElements();
